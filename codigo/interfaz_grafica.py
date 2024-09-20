e6b2ca0a2236287e72c97ff765212ac4c565ca3d
Diana <dianasccuni@gmail.com>
2024-09-20 15:13:30 -0600

    intefaz modificada

--- codigo/interfaz_grafica.py
+++ codigo/interfaz_grafica.py
@@ -1,10 +1,21 @@
-import sys, pygame 
+import sys
+import pygame 
 pygame.init()
-ancho , altura = 800, 600
-screen = pygame.display.set_mode(ancho,altura)
+#PANTALLA
+width = 800
+height = 600
+screen = pygame.display.set_mode((width, height))  
 pygame.display.set_caption("JUEGO CRUCIGRAMA3D")
+icon = pygame.image.load("icon.png")
+pygame.display.set_icon(icon)
+
+#LOOP DE LA PANTALLA
+
 run = True 
 while run:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: run = False
+        if event.type == pygame.QUIT:
+            run = False
+
 pygame.quit()
+
