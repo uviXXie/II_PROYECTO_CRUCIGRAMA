09f1b5a90780bb56c69ad347f42ea257539acc11
Diana <dianasccuni@gmail.com>
2024-09-21 00:50:48 -0600

    load de icon  y dibujo de botones

--- codigo/interfaz_grafica.py
+++ codigo/interfaz_grafica.py
@@ -1,21 +1,39 @@
 import sys
 import pygame 
+import os
+print(os.getcwd())
+
 pygame.init()
 #PANTALLA
-width = 800
-height = 600
+width = 550
+height = 300
 screen = pygame.display.set_mode((width, height))  
-pygame.display.set_caption("JUEGO CRUCIGRAMA3D")
-icon = pygame.image.load("icon.png")
+pygame.display.set_caption("CRUCIGRAMA3D")
+icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
-
+##########dibujos#######
+title = pygame.Rect(100,75, 400,45)
+##########BOTONES###########
+start_button = pygame.Rect(270,74,45,175)
+exit_button = pygame.Rect(170,204,146,45)
 #LOOP DE LA PANTALLA
 
-run = True 
+run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    #3 valores rgb
+    screen.fill((255, 199, 188))
+    pygame.draw.rect(screen,(58, 188, 252 ), title)
+    pygame.draw.rect(screen,(52, 148, 252 ), start_button)
+    pygame.draw.rect(screen,(252, 52, 75 ), exit_button)
+    exit_message = fuente.render("EXIT",True,(0, 0, 0 ))
+    start_message = fuente.render("START",True,(0, 0, 0  ))
+    screen.blit
+
+    pygame.display.update()
+
 
 pygame.quit()
 
